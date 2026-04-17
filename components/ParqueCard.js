9db068b97e7f77a1da49b6f7c30a5fa30741e602
Eduardo Paes <eduardo.l.p05@gmail.com>
2026-04-17 20:35:18 -0300

    feat: implementação do portfólio de parques nacionais

---
+++ components/ParqueCard.js
@@ -0,0 +1,41 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+// Usamos uma arrow function e desestruturamos as props para pegar diretamente titulo e descricao [cite: 31, 64]
+const ParqueCard = ({ titulo, descricao }) => {
+  return (
+    <View style={styles.card}>
+      <Text style={styles.titulo}>{titulo}</Text>
+      <Text style={styles.descricao}>{descricao}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: '#ffffff', // Fundo branco sólido
+    padding: 20,
+    marginVertical: 12,
+    marginHorizontal: 16,
+    borderRadius: 16, // Bordas bem arredondadas
+    
+    // Configurações de sombra para dar um efeito sutil de elevação (Flat Design)
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.05,
+    shadowRadius: 8,
+    elevation: 3, // Sombra no Android
+  },
+  titulo: {
+    fontSize: 22,
+    fontWeight: '700',
+    color: '#2d3436',
+    marginBottom: 8,
+  },
+  descricao: {
+    fontSize: 16,
+    color: '#636e72',
+    lineHeight: 24, // Aumenta o espaçamento entre linhas para melhor legibilidade
+  }
+});
+
+export default ParqueCard;
